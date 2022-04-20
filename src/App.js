1a3cf52f34d00a75a951989c5943d2d86cce98f1
Cosmin Adrian <cosminadrian218@gmail.com>
2022-04-20 14:12:50 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
   render() {
     return (
