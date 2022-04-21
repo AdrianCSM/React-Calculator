1c5a8db6eb90b71758897feba5175e9efc648dab
Cosmin Adrian <cosminadrian218@gmail.com>
2022-04-21 22:27:46 +0300

    Implement functionality

---
+++ src/components/buttons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class CalcButton extends React.Component {
+  render() {
+    const { buttonName, buttonClasses, myFunc } = this.props;
+    return (
+      <button type="button" onClick={myFunc} className={buttonClasses}>{buttonName}</button>
+    );
+  }
+}
+
+CalcButton.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+  buttonClasses: PropTypes.string.isRequired,
+  myFunc: PropTypes.func.isRequired,
+};
